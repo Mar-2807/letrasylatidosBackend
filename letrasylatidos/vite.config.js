e5cb0f60c0bb9aeb27f7c55f29144e5340d8738f
Marely <167366049+Mare-2807@users.noreply.github.com>
2025-02-11 23:46:47 -0600

    Concurrently

--- letrasylatidos/vite.config.js
+++ letrasylatidos/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 4173, // Usa el puerto de la variable de entorno de Render
+    port: process.env.PORT || 4173, // Usar el puerto de Render, si está disponible
+    host: '0.0.0.0', // Permite que la aplicación sea accesible desde fuera del contenedor
   },
-})
+});
